a8674dc02102e0af5beca988580a4aae77441d5c
welder496 <welder496@gmail.com>
2016-04-04 15:14:25 -0300

    Atualização com a exclusão da biblioteca persistence-api

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import model.Recurso;
 import model.Usuario;
 
 public class Application {
@@ -14,11 +15,24 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		if (em != null) {
-		/*	Usuario usr = new Usuario();
+			Usuario usr = new Usuario();
+			em.getTransaction().begin();		
 			usr.setNome("Welder Maurício de Souza");
-			em.getTransaction().begin();
-			em.persist(usr);
-			em.getTransaction().commit(); */
+			
+			Recurso r1 = new Recurso();
+			r1.setRecurso("home/welder/teste");
+			usr.addRecurso(r1);
+			
+			Recurso r2 = new Recurso();
+			r2.setRecurso("home/welder/app");
+			usr.addRecurso(r2);
+
+			Recurso r3 = new Recurso();
+			r3.setRecurso("home/welder/app");
+			usr.addRecurso(r3);
+			
+			em.persist(usr);		
+			em.getTransaction().commit(); 
 		} else {
 			System.out.println("Não foi possível fazer conexão!!");
 		}		
--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,11 +1,16 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario implements Serializable {
@@ -21,6 +26,10 @@ public class Usuario implements Serializable {
 	
 	@Column(name = "nome")
 	private String nome;
+
+	@OneToMany(targetEntity=Recurso.class, mappedBy="usuario", fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	private List<Recurso> recursos = new ArrayList<Recurso>();
+		
 	
 	public String getNome() {
 		return nome;
@@ -28,6 +37,15 @@ public class Usuario implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Recurso> getRecursos() {
+		return recursos;
+	}
+
+	public void addRecurso(Recurso recurso){
+		recurso.setUsuario(this);
+		recursos.add(recurso);
 	}
 	
 }
